7dd63817d5388c0d3b9b061d2deef193dc1399a6
katie0109 <0109katie@naver.com>
2026-03-05 11:34:12 +0900

    feat: 칸반보드 태스크 카드 예제 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,6 +1,7 @@
 package woowacourse.kanban.board
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -25,9 +26,25 @@ fun App() {
                 .safeContentPadding()
                 .fillMaxSize(),
         ) {
-            Row{
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(18.5.dp)
+            ){
                 val tagNames = listOf("컴포넌트", "성능")
                 TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames, "다이노")
+
+                val tagNames02 = listOf("컴포넌트", "성능")
+                TaskCard("LazyColumn 컴포넌트 구현", "", tagNames02, "다이노")
+
+                val tagNames03 = emptyList<String>()
+                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames03, "다이노")
+
+                val tagNames04 = emptyList<String>()
+                TaskCard("LazyColumn 컴포넌트 구현", ".", tagNames04, "다이노")
+
+                val tagNames05 = listOf("너무너무", "성능", "긴 태그", "최대로", "5자까지", "5개제한임")
+                TaskCard("너무너무긴제목은 한줄까지만 노출됩니다.", "너무너무 긴 설명은 두 줄깢만 노출하고 말줄임표로 처리합니다. 너무너무 긴 설명은 두 줄깢만 노출하고 말줄임표로 처리합니다.", tagNames05, "너무너무너무 긴 담당자도 한 줄로 표시됩니다.")
+
+
             }
 
             }
