166fe5adad9d2d3c22d21748f66a17e14bc37ba0
katie0109 <0109katie@naver.com>
2026-03-06 16:54:28 +0900

    fix:더미데이터 별도 분리

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -13,10 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.data.TaskCardData
 import woowacourse.kanban.board.task.TaskCard
+
 
 @Composable
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+fun AppPreview() {
+    App()
+}
+
+@Composable
 fun App() {
     MaterialTheme {
         Column(
@@ -25,35 +32,33 @@ fun App() {
                 .padding(horizontal = 62.5.dp, vertical = 60.dp)
                 .safeContentPadding()
                 .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(18.5.dp)
+            verticalArrangement = Arrangement.spacedBy(18.5.dp),
         ) {
-            Row (
-                horizontalArrangement = Arrangement.spacedBy(18.5.dp)
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(18.5.dp),
             ) {
-                val tagNames01 = listOf("컴포넌트", "성능")
-                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames01, "다이노")
-
-                val tagNames02 = listOf("컴포넌트", "성능")
-                TaskCard("LazyColumn 컴포넌트 구현", "", tagNames02, "다이노")
-
-                val tagNames03 = emptyList<String>()
-                TaskCard("LazyColumn 컴포넌트 구현", "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.", tagNames03, "다이노")
-
-                val tagNames04 = emptyList<String>()
-                TaskCard("LazyColumn 컴포넌트 구현", ".", tagNames04, "다이노")
+                TaskCardData.tasks.take(4).forEach { task ->
+                    TaskCard(
+                        title = task.title,
+                        contents = task.contents,
+                        tagNames = task.tagNames,
+                        contactName = task.contactName,
+                    )
+                }
 
             }
 
-            Row () {
-                val tagNames05 = listOf("너무너무", "성능", "긴 태그", "최대로", "5자까지", "5개제한임")
+            Row {
+                val maxTask = TaskCardData.tasks.last()
                 TaskCard(
-                    "너무너무긴제목은 한줄까지만 노출됩니다.",
-                    "너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다. 너무너무 긴 설명은 두 줄깢만 노출하고 말줄임표로 처리합니다.",
-                    tagNames05,
-                    "너무너무너무 긴 담당자도 한 줄로 표시됩니다.",
+                    title = maxTask.title,
+                    contents = maxTask.contents,
+                    tagNames = maxTask.tagNames,
+                    contactName = maxTask.contactName,
                 )
             }
 
         }
     }
 }
+
